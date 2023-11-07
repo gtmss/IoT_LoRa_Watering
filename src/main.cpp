770787984b663f5abecd66f7609cb38b9fc87606
gtm <ion.gatman@esempla.com>
2023-11-07 11:44:31 +0200

    lora gateway

--- src/main.cpp
+++ src/main.cpp
@@ -1,161 +1,150 @@
-/* Heltec Automation Ping Pong communication test example
- *
- * Function:
- * 1. Ping Pong communication in two CubeCell device.
- * 
- * Description:
- * 1. Only hardware layer communicate, no LoRaWAN protocol support;
- * 2. Download the same code into two CubeCell devices, then they will begin Ping Pong test each other;
- * 3. This example is for CubeCell hardware basic test.
- *
- * HelTec AutoMation, Chengdu, China
- * 成都惠利特自动化科技有限公司
- * www.heltec.org
- *
- * this project also realess in GitHub:
- * https://github.com/HelTecAutomation/ASR650x-Arduino
- * */
-
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
 
 /*
- * set LoraWan_RGB to 1,the RGB active in loraWan
+ * set LoraWan_RGB to Active,the RGB active in loraWan
  * RGB red means sending;
+ * RGB purple means joined done;
+ * RGB blue means RxWindow1;
+ * RGB yellow means RxWindow2;
  * RGB green means received done;
  */
-#ifndef LoraWan_RGB
-#define LoraWan_RGB 1
-#endif
 
-#define RF_FREQUENCY                                868000000 // Hz
+/* OTAA para*/
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x24, 0x36 };
+uint8_t appEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x24, 0x36 };
+uint8_t appKey[] = { 0xA3, 0x7F, 0xF7, 0x90, 0x4A, 0x91, 0xE5, 0xD8, 0xAA, 0x86, 0x34, 0xEF, 0x26, 0x0D, 0xE1, 0xDD };
 
-#define TX_OUTPUT_POWER                             5        // dBm
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x9E, 0x83, 0x97, 0xFB, 0x7C, 0x92, 0xF3, 0x66, 0xAE, 0x42, 0x16, 0xA2, 0x4A, 0xA5, 0x4E, 0xD3 };
+uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t devAddr =  ( uint32_t )0x260B559B;
 
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-                                                              //  1: 250 kHz,
-                                                              //  2: 500 kHz,
-                                                              //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-                                                              //  2: 4/6,
-                                                              //  3: 4/7,
-                                                              //  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
+/*LoraWan channelsmask, default channels 0-7*/ 
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
 
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
 
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 30 // Define the payload size here
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = LORAWAN_CLASS;
 
-char txpacket[BUFFER_SIZE];
-char rxpacket[BUFFER_SIZE];
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
 
-static RadioEvents_t RadioEvents;
-void OnTxDone( void );
-void OnTxTimeout( void );
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+/*OTAA or ABP*/
+bool overTheAirActivation = LORAWAN_NETMODE;
 
-typedef enum
+/*ADR enable*/
+bool loraWanAdr = LORAWAN_ADR;
+
+/* set LORAWAN_Net_Reserve ON, the node could save the network info to flash, when node reset not need to join again */
+bool keepNet = LORAWAN_NET_RESERVE;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = LORAWAN_UPLINKMODE;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
 {
-    LOWPOWER,
-    RX,
-    TX
-}States_t;
-
-int16_t txNumber;
-States_t state;
-bool sleepMode = false;
-int16_t Rssi,rxSize;
+	/*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+	*for example, if use REGION_CN470, 
+	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+	*/
+    appDataSize = 4;
+    appData[0] = 0x00;
+    appData[1] = 0x01;
+    appData[2] = 0x02;
+    appData[3] = 0x03;
+}
 
 
 void setup() {
-    boardInitMcu( );
-    Serial.begin(115200);
-
-    txNumber=0;
-    Rssi=0;
-
-    RadioEvents.TxDone = OnTxDone;
-    RadioEvents.TxTimeout = OnTxTimeout;
-    RadioEvents.RxDone = OnRxDone;
-
-    Radio.Init( &RadioEvents );
-    Radio.SetChannel( RF_FREQUENCY );
-    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
-                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
-                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
-
-    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
-                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
-                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
-    state=TX;
+	Serial.begin(115200);
+#if(AT_SUPPORT)
+	enableAt();
+#endif
+	deviceState = DEVICE_STATE_INIT;
+	LoRaWAN.ifskipjoin();
 }
-
-
 
 void loop()
 {
-	switch(state)
+    const char* messageToSend = "Hello, from here!";
+
+	switch( deviceState )
 	{
-		case TX:
-			delay(1000);
-			txNumber++;
-		    sprintf(txpacket,"%s","hello");
-		    sprintf(txpacket+strlen(txpacket),"%d",txNumber);
-		    sprintf(txpacket+strlen(txpacket),"%s"," Rssi : ");
-		    sprintf(txpacket+strlen(txpacket),"%d",Rssi);
-		    turnOnRGB(COLOR_SEND,0);
-
-		    Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
-
-		    Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
-		    state=LOWPOWER;
-		    break;
-		case RX:
-			Serial.println("into RX mode");
-		    Radio.Rx( 0 );
-		    state=LOWPOWER;
-		    break;
-		case LOWPOWER:
-			lowPowerHandler();
-		    break;
-        default:
-            break;
+		case DEVICE_STATE_INIT:
+		{
+#if(LORAWAN_DEVEUI_AUTO)
+			LoRaWAN.generateDeveuiByChipID();
+#endif
+#if(AT_SUPPORT)
+			getDevParam();
+#endif
+			printDevParam();
+			LoRaWAN.init(loraWanClass,loraWanRegion);
+			deviceState = DEVICE_STATE_JOIN;
+			break;
+		}
+		case DEVICE_STATE_JOIN:
+		{
+			LoRaWAN.join();
+			break;
+		}
+		case DEVICE_STATE_SEND:
+		{
+			prepareTxFrame( appPort );
+			LoRaWAN.send(messageToSend, strlen(messageToSend));
+			deviceState = DEVICE_STATE_CYCLE;
+			break;
+		}
+		case DEVICE_STATE_CYCLE:
+		{
+			// Schedule next packet transmission
+			txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+			LoRaWAN.cycle(txDutyCycleTime);
+			deviceState = DEVICE_STATE_SLEEP;
+			break;
+		}
+		case DEVICE_STATE_SLEEP:
+		{
+			LoRaWAN.sleep();
+			break;
+		}
+		default:
+		{
+			deviceState = DEVICE_STATE_INIT;
+			break;
+		}
 	}
-    Radio.IrqProcess( );
-}
-
-void OnTxDone( void )
-{
-	Serial.print("TX done......");
-	turnOnRGB(0,0);
-	state=RX;
-}
-
-void OnTxTimeout( void )
-{
-    Radio.Sleep( );
-    Serial.print("TX Timeout......");
-    state=TX;
-}
-void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
-{
-    Rssi=rssi;
-    rxSize=size;
-    memcpy(rxpacket, payload, size );
-    rxpacket[size]='\0';
-    turnOnRGB(COLOR_RECEIVED,0);
-    Radio.Sleep( );
-
-    Serial.printf("\r\nreceived packet \"%s\" with Rssi %d , length %d\r\n",rxpacket,Rssi,rxSize);
-    Serial.println("wait to send next packet");
-
-    state=TX;
 }
 
 void readSoilMoisture() {
